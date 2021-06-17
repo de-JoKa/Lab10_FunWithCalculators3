7de47288d890bed8e2b3a4897181b045879903a2
Maryna Kuzminchuk <Maryna.Kuzminchuk@Student.HTW-Berlin.de>
2021-06-17 21:54:37 +0200

    change UserInterfaceSet

--- src/UserInterfaceSet.java
+++ src/UserInterfaceSet.java
@@ -5,53 +5,73 @@ import java.awt.event.ActionEvent;
 
 public class UserInterfaceSet extends UserInterface {
 
-	JTextField displaySet2;
-	private JTextField displayResult;
-	CalcEngineSet calcSet;
-	
-	public UserInterfaceSet(CalcEngineSet engine) {
-		super(engine);
-		calcSet = engine;
-		// TODO Auto-generated constructor stub
-	}
-	
-	protected void makeFrame() {
+    private JTextField input;
+    private JTextField setA;
+    private JTextField setB;
+    private JTextField result;
+    CalcEngineSet calcSet;
+
+    public UserInterfaceSet(CalcEngineSet engine) {
+        super(engine);
+        calcSet = engine;
+        // TODO Auto-generated constructor stub
+    }
+
+    protected void makeFrame() {
         frame = new JFrame(calc.getTitle());
-        
-        JPanel contentPane = (JPanel)frame.getContentPane();
-        contentPane.setLayout(new BorderLayout(8, 8));
-        contentPane.setBorder(new EmptyBorder( 10, 10, 10, 10));
 
-        display = new JTextField();
-        contentPane.add(display, BorderLayout.NORTH);
+        JPanel contentPane = (JPanel) frame.getContentPane();
+        contentPane.setLayout(new GridLayout(8, 2));
+        contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
 
-        JPanel buttonPanel = new JPanel(new GridLayout(4, 2));
-            addButton(buttonPanel, "Union");
-            addButton(buttonPanel, "Intersection");
-            addButton(buttonPanel, "Subtraction");
-            addButton(buttonPanel, "Clear");
-            
-            addButton(buttonPanel, "Push Set A");
-            addButton(buttonPanel, "Push Set B");
-            addButton(buttonPanel, "Length Set A");
-            addButton(buttonPanel, "Length Set B");
-            
-//            addButton(buttonPanel, "0");
-//            addButton(buttonPanel, "+");
-//            addButton(buttonPanel, "-");
-//            addButton(buttonPanel, "*");
-//            addButton(buttonPanel, "=");
-//            
-        contentPane.add(buttonPanel, BorderLayout.CENTER);
+        JLabel inputLabel = new JLabel("Input");
+        contentPane.add(inputLabel);
+        input = new JTextField();
+        contentPane.add(input);
+
+        addButton(contentPane, "Push to Set A");
+        addButton(contentPane, "Push to Set B");
+
+        JLabel setALabel = new JLabel("Set A");
+        contentPane.add(setALabel);
+        setA = new JTextField();
+        contentPane.add(setA);
+
+        JLabel setBLabel = new JLabel("Set B");
+        contentPane.add(setBLabel);
+        setB = new JTextField();
+        contentPane.add(setB);
+//        JPanel buttonPanel = new JPanel(new GridLayout(4, 2));
+        addButton(contentPane, "Union");
+        addButton(contentPane, "Intersection");
+        addButton(contentPane, "Subtraction");
+        addButton(contentPane, "Clear");
+
+        JLabel resultLabel = new JLabel("Result");
+        contentPane.add(resultLabel);
+        result = new JTextField();
+        contentPane.add(result);
 
         status = new JLabel(calc.getAuthor());
-        contentPane.add(status, BorderLayout.SOUTH);
+        contentPane.add(status);
+
+//
+////            addButton(buttonPanel, "0");
+////            addButton(buttonPanel, "+");
+////            addButton(buttonPanel, "-");
+////            addButton(buttonPanel, "*");
+////            addButton(buttonPanel, "=");
+////
+//        contentPane.add(buttonPanel, BorderLayout.CENTER);
+//
+//        status = new JLabel(calc.getAuthor());
+//        contentPane.add(status, BorderLayout.SOUTH);
 
         frame.pack();
-	}
-	
-	public void actionPerformed(ActionEvent event) {
-		
-	}
+    }
+
+    public void actionPerformed(ActionEvent event) {
+
+    }
 
 }
